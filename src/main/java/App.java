6f5d74fcd28595fc3b868f7e4afa87f8371cba68
Jeffrey Ruder <ruderjt@gmail.com>
2016-01-12 19:57:23 -0800

    Add basic route to App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,5 +7,25 @@ import static spark.Spark.*;
 public class App {
     public static void main(String[] args) {
 
+        //RESTful ARCHITECTURE
+        //Use POST to create something on the server
+        //Use GET to retrieve something from the server
+        //Use PUT to change or update something on the server
+        //Use DELETE to remove or delete something on the server
+        //Keep URLs intuitive
+        //Each request from client contains all info necessary for that request
+
+        //ROUTES: Home Page
+
+        get("/", (request, response) -> {
+            HashMap<String, Object> model = new HashMap<String, Object>();
+            model.put("template", "templates/index.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+        //ROUTES: Identification of Resources
+
+        //ROUTES: Changing Resources
+
     }
 }
