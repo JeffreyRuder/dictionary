83808cff1b2aabb0cc933192038d982030f77f08
Jeffrey Ruder <ruderjt@gmail.com>
2016-01-12 19:37:22 -0800

    Add boilerplate import statements to App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,11 @@
+import java.util.HashMap;
+
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+
 public class App {
     public static void main(String[] args) {
-        
+
     }
 }
