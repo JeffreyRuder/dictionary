5f6d7d3caea2157fd8ae0767e42930cebd3dce98
Jeffrey Ruder <ruderjt@gmail.com>
2016-01-15 08:23:28 -0800

    Add Spark routing for get and post requests

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -30,16 +30,60 @@ public class App {
 
         //ROUTES: Home Page
 
-        // get("/", (request, response) -> {
-        //     HashMap<String, Object> model = new HashMap<String, Object>();
-
-        //     model.put("template", "templates/index.vtl");
-        //     return new ModelAndView(model, layout);
-        // }, new VelocityTemplateEngine());
+        get("/", (request, response) -> {
+            HashMap<String, Object> model = new HashMap<String, Object>();
+            model.put("words", Words.getAll());
+            model.put("template", "templates/allwords.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
 
         //ROUTES: Identification of Resources
 
+        get("/new", (request, response) -> {
+            HashMap<String, Object> model = new HashMap<String, Object>();
+            model.put("template", "templates/newword.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+        get("/:id", (request, response) -> {
+          HashMap<String, Object> model = new HashMap<String, Object>();
+          model.put("word", Word.getWord(request.params(":id")));
+          model.put("template", "templates/word.vtl");
+          return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
         //ROUTES: Changing Resources
+
+        post("/", (request, response) -> {
+          HashMap<String, Object> model = new HashMap<String, Object>();
+          boolean alreadyAdded = false;
+          String userWord = request.queryParams("userword");
+
+          if (Word.getWord(userWord) != null) {
+            alreadyAdded = true;
+          } else {
+            Word newWord = new Word(userWord);
+          }
+
+          model.put("alreadyadded", alreadyAdded);
+          model.put("words", Words.getAll());
+          model.put("template", "templates/allwords.vtl");
+          return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
+
+      post("/:id", (request, response) -> {
+          HashMap<String, Object> model = new HashMap<String, Object>();
+          Word thisWord = Word.getWord(request.params(":id"));
+          String userDefinition = request.queryParams("userdefinition");
+          String userExample = request.queryParams("userexample");
+          Definition newDefinition = new Definition(userDefinition, userExample);
+
+          thisWord.addDefinition(newDefinition);
+
+          model.put("word", thisWord);
+          model.put("template", "templates/word.vtl");
+          return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
 
     }
 }
